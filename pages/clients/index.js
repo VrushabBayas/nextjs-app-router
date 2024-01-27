e78fa67bc036585ce2a8f78f3d3967d9d3f3aecb
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-27 18:55:52 +0530

    Add client list with links to individual client pages

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 export default function ClientPage() {
-  return <h1>Main ClientPage</h1>;
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manual", name: "Manual" },
+    { id: "vrush", name: "Vrushabh" },
+  ];
+  return (
+    <>
+      <h1>Main ClientPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
