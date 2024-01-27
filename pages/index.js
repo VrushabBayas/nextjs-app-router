4fd03b71639cf493bf147b7f80cdc58c44c81bea
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-27 18:51:03 +0530

    Add navigation links to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
