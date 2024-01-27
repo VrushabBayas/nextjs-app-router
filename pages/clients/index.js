68a53325bac28bc7c44af576a6d571f9bca22300
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-27 18:57:46 +0530

    Update client page links to use dynamic routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -13,7 +13,15 @@ export default function ClientPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
